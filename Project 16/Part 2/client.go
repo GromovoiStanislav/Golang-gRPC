1b8896de459b9604a19dd3bfff7abda3ce29024b
Gromovoi Stanislav <gromila12@rambler.ru>
2023-11-03 13:31:44 +0600

    Project 16

--- Project 16/Part 2/client.go
+++ Project 16/Part 2/client.go
@@ -11,6 +11,7 @@ import (
 	pb "main/proto/usermgmt"
 )
 
+
 const (
 	address = "localhost:50051"
 )
@@ -26,9 +27,11 @@ func main() {
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
 	var new_users = make(map[string]int)
 	new_users["Alice"] = 43
 	new_users["Bob"] = 30
+
 	for name, age := range new_users {
 		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: int32(age)})
 		if err != nil {
@@ -40,8 +43,9 @@ AGE: %d
 ID: %d`, r.GetName(), r.GetAge(), r.GetId())
 
 	}
-	params := &pb.GetUsersParams{}
-	r, err := c.GetUsers(ctx, params)
+
+	
+	r, err := c.GetUsers(ctx, &pb.GetUsersParams{})
 	if err != nil {
 		log.Fatalf("could not create user: %v", err)
 	}
