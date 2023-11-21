39cb30da9ef321752989293dd5b2c813a1a33246
Gromovoi Stanislav <gromila12@rambler.ru>
2023-11-21 10:45:22 +0600

    Project 33

---
+++ Project 33/client/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	ecpb "multiplex/echo"
+	hwpb "multiplex/helloworld"
+)
+
+var addr = flag.String("addr", "localhost:50051", "the address to connect to")
+
+
+func callSayHello(client hwpb.GreeterClient, name string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	r, err := client.SayHello(ctx, &hwpb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("client.SayHello(_) = _, %v", err)
+	}
+	fmt.Println("Greeting: ", r.Message)
+}
+
+func callUnaryEcho(client ecpb.EchoClient, message string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	resp, err := client.UnaryEcho(ctx, &ecpb.EchoRequest{Message: message})
+	if err != nil {
+		log.Fatalf("client.UnaryEcho(_) = _, %v: ", err)
+	}
+	fmt.Println("UnaryEcho: ", resp.Message)
+}
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("--- calling helloworld.Greeter/SayHello ---")
+	// Make a greeter client and send an RPC.
+	hwc := hwpb.NewGreeterClient(conn)
+	callSayHello(hwc, "multiplex")
+
+	fmt.Println()
+	
+	fmt.Println("--- calling echo.Echo/UnaryEcho ---")
+	// Make a routeguild client with the same ClientConn.
+	rgc := ecpb.NewEchoClient(conn)
+	callUnaryEcho(rgc, "this is examples/multiplex")
+}
---
+++ Project 33/server/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+
+	ecpb "multiplex/echo"
+	hwpb "multiplex/helloworld"
+)
+
+var port = flag.Int("port", 50051, "the port to serve on")
+
+// hwServer is used to implement helloworld.GreeterServer.
+type hwServer struct {
+	hwpb.UnimplementedGreeterServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *hwServer) SayHello(ctx context.Context, req *hwpb.HelloRequest) (*hwpb.HelloReply, error) {
+	return &hwpb.HelloReply{Message: "Hello " + req.Name}, nil
+}
+
+type ecServer struct {
+	ecpb.UnimplementedEchoServer
+}
+
+func (s *ecServer) UnaryEcho(ctx context.Context, req *ecpb.EchoRequest) (*ecpb.EchoResponse, error) {
+	return &ecpb.EchoResponse{Message: req.Message}, nil
+}
+
+func main() {
+	flag.Parse()
+	
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	fmt.Printf("server listening at %v\n", lis.Addr())
+
+	s := grpc.NewServer()
+
+	// Register Greeter on the server.
+	hwpb.RegisterGreeterServer(s, &hwServer{})
+
+	// Register RouteGuide on the same server.
+	ecpb.RegisterEchoServer(s, &ecServer{})
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
